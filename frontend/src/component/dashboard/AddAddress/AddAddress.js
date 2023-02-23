04b9369a7b63f440145e7f11633948e32d50e06e
navjeet4office <navjeet.immanent@gmail.com>
2023-02-23 12:39:21 +0530

    updated

--- frontend/src/component/dashboard/AddAddress/AddAddress.js
+++ frontend/src/component/dashboard/AddAddress/AddAddress.js
@@ -1,17 +1,14 @@
-import React from 'react'
-import "./AddAddress.css"
-
+import React from "react";
+import "./AddAddress.css";
 
 const AddAddress = () => {
   return (
     <>
-            <div className='PropertyAddress'>
-
-                
-            </div>
-
+      <div className="PropertyAddress">
+        
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default AddAddress
+export default AddAddress;
